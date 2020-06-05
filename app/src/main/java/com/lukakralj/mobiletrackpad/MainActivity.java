8021cffea3c75b4ff6316249a9bd0659e0ce4a53
lukakralj <luka.kralj2@gmail.com>
2020-06-05 23:36:39 +0200

    attempt multipointer refactor

--- app/src/main/java/com/lukakralj/mobiletrackpad/MainActivity.java
+++ app/src/main/java/com/lukakralj/mobiletrackpad/MainActivity.java
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.TextView;
 import com.lukakralj.mobiletrackpad.backend.RequestCode;
 import com.lukakralj.mobiletrackpad.backend.ServerConnection;
@@ -19,18 +18,26 @@ import com.lukakralj.mobiletrackpad.backend.ServerConnection;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static int clickDistanceThreshold = 5;
-    private static long longPressThreshold = 300; // ms
+    private static int scrollDistanceThreshold = 5;
+    private static float scrollFactor = 10; // controls speed of scrolling
+    private static long longPressThreshold = 250; // ms
 
     private TextView text;
-    private Position movedStartPos;
-    private Position prevPos;
+
+    private HashMap<Integer, PointerData> activePointers;
+    private int mainPointerId;
+    private ArrayList<Integer> otherPointers; // non main ones, in order
+
     private boolean mouseMoving;
     private boolean mouseDragged;
+    private boolean isDoubleTouch;
     private boolean isLeftDown;
     private long startMillis;
     private Handler handler; // UI handler
@@ -42,10 +49,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = (TextView) findViewById(R.id.text);
-        prevPos =  null;
-        movedStartPos = null;
+
+        activePointers = new HashMap<>(3);
+        mainPointerId = -1;
+        otherPointers = new ArrayList<>();
+
         mouseMoving = false;
         mouseDragged = false;
+        isDoubleTouch = false;
+
         isLeftDown = false;
         startMillis = 0;
         handler = new Handler(Looper.getMainLooper());
@@ -92,57 +104,144 @@ public class MainActivity extends AppCompatActivity {
     public boolean onTouchEvent(MotionEvent event) {
         int x = (int) event.getX();
         int y = (int) event.getY();
+        int id = event.getPointerId(event.getActionIndex());
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                movedStartPos = new Position(x, y);
-                prevPos = new Position(x, y);
-                mouseMoving = false;
-                startMillis = System.currentTimeMillis();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (Math.abs(x - movedStartPos.x) < clickDistanceThreshold &&
-                        Math.abs(y - movedStartPos.y) < clickDistanceThreshold &&
-                        System.currentTimeMillis() - startMillis >= longPressThreshold) {
-                    leftDown();
-                    mouseDragged = true;
-                    isLeftDown = true;
+                if (mainPointerId == -1) {
+                    startMillis = System.currentTimeMillis();
                 }
 
-                if (Math.abs(x - movedStartPos.x) >= clickDistanceThreshold ||
-                        Math.abs(y - movedStartPos.y) >= clickDistanceThreshold) {
-                    mouseMoving = true;
-                    mouseDelta(x - prevPos.x, y - prevPos.y);
-                    prevPos.x = x;
-                    prevPos.y = y;
+                initPointer(id, x, y);
+
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+                if (!mouseDragged) {
+                    System.out.println("Down: " + id);
+                    initPointer(id, x, y);
+                    isDoubleTouch = true;
                 }
                 break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (id == mainPointerId) {
+                    PointerData main = activePointers.get(id);
+                    int dxStart = x - main.moveStartPos.x;
+                    int dyStart = y - main.moveStartPos.y;
+                    int dx = x - main.prevPos.x;
+                    int dy = y - main.prevPos.y;
+
+                    if (isDoubleTouch) {
+                        // Scroll
+                        if (Math.abs(dxStart) >= scrollDistanceThreshold ||
+                                Math.abs(dyStart) >= scrollDistanceThreshold) {
+                            mouseMoving = true;
+
+                            scrollX(dx);
+                            //TODO: dy will be used for horizontal scroll eventually
+
+                            main.prevPos.x = x;
+                            main.prevPos.y = y;
+                        }
+                    }
+                    else {
+                        // Check if main pointer is ready to drag
+                        if (Math.abs(dxStart) < clickDistanceThreshold &&
+                                Math.abs(dyStart) < clickDistanceThreshold &&
+                                System.currentTimeMillis() - startMillis >= longPressThreshold) {
+                            leftDown();
+                            mouseDragged = true;
+                            isLeftDown = true;
+                        }
+
+                        // Move or drag mouse
+                        if (Math.abs(dxStart) >= clickDistanceThreshold ||
+                                Math.abs(dyStart) >= clickDistanceThreshold) {
+                            mouseMoving = true;
+                            mouseDelta(dx, dy);
+                            main.prevPos.x = x;
+                            main.prevPos.y = y;
+                        }
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                System.out.println("Up: " + id);
             case MotionEvent.ACTION_UP:
-                if (!mouseMoving && !mouseDragged
-                        && Math.abs(x - movedStartPos.x) < clickDistanceThreshold
-                        && Math.abs(y - movedStartPos.y) < clickDistanceThreshold) {
+                if (id == mainPointerId && !mouseMoving && !mouseDragged && !isDoubleTouch
+                        && Math.abs(x - activePointers.get(id).moveStartPos.x) < clickDistanceThreshold
+                        && Math.abs(y - activePointers.get(id).moveStartPos.y) < clickDistanceThreshold) {
                     leftClick();
                 }
-                resetMouse();
+                else if (otherPointers.size() == 1 && !mouseMoving && !mouseDragged && isDoubleTouch) {
+                    PointerData main = activePointers.get(mainPointerId);
+                    PointerData secondPointer = activePointers.get(otherPointers.get(0));
+
+                    if (Math.abs(x - main.moveStartPos.x) < clickDistanceThreshold
+                            && Math.abs(y - main.moveStartPos.y) < clickDistanceThreshold
+                            && Math.abs(x - secondPointer.moveStartPos.x) < clickDistanceThreshold
+                            && Math.abs(y - secondPointer.moveStartPos.y) < clickDistanceThreshold) {
+                        rightClick();
+                    }
+                }
+
+                deletePointer(id);
                 break;
+
             case MotionEvent.ACTION_CANCEL:
-                resetMouse();
+                deletePointer(id);
                 break;
         }
 
         return false;
     }
 
-    private void resetMouse() {
-        mouseMoving = false;
-        movedStartPos = null;
-        prevPos = null;
-        startMillis = 0;
-        mouseDragged = false;
-        if (isLeftDown) {
-            leftUp();
+    private void initPointer(int id, int x, int y) {
+        activePointers.put(id, new PointerData(new Position(x,y), new Position(x,y)));
+
+        if (mainPointerId == -1) {
+            mainPointerId = id;
+        }
+        else {
+            otherPointers.add(id);
+            if (!mouseDragged) {
+                isDoubleTouch = true;
+            }
         }
     }
+
+    private void deletePointer(int id) {
+        activePointers.remove(id);
+
+        if (mainPointerId == id) {
+            if (!otherPointers.isEmpty()) {
+                mainPointerId = otherPointers.remove(0);
+                startMillis = System.currentTimeMillis();
+            }
+            else {
+                mainPointerId = -1;
+            }
+            mouseMoving = false;
+            mouseDragged = false;
+
+            if (otherPointers.isEmpty()) {
+                isDoubleTouch = false;
+            }
+            else {
+                isDoubleTouch = true;
+            }
+
+            if (isLeftDown) {
+                leftUp();
+            }
+        }
+        else {
+            startMillis = System.currentTimeMillis();
+        }
+    }
+
 
     private void mouseDelta(int deltaX, int deltaY) {
         if ((deltaX == 0) && deltaY == 0) {
@@ -178,12 +277,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void scrollUp() {
-        ServerConnection.getInstance().scheduleRequest(RequestCode.SCROLL_UP, data -> {});
+    private void scrollX(int dx) {
+        if (scrollFactor == 0) {
+            scrollFactor = 1;
+        }
+        int amount = (int)(Math.ceil(Math.abs(dx) / scrollFactor));
+
+        if (dx > 0) {
+            scrollUp(amount);
+        }
+        else if (dx < 0) {
+            scrollDown(amount);
+        }
     }
 
-    private void scrollDown() {
-        ServerConnection.getInstance().scheduleRequest(RequestCode.SCROLL_DOWN, data -> {});
+    private void scrollUp(int amount) {
+        JSONObject extraData = new JSONObject();
+        try {
+            extraData.put("amount", amount);
+        }
+        catch (JSONException e) {
+            System.out.println(e);
+            return;
+        }
+
+        ServerConnection.getInstance().scheduleRequest(RequestCode.SCROLL_UP, extraData, data -> {});
+    }
+
+    private void scrollDown(int amount) {
+        JSONObject extraData = new JSONObject();
+        try {
+            extraData.put("amount", amount);
+        }
+        catch (JSONException e) {
+            System.out.println(e);
+            return;
+        }
+
+        ServerConnection.getInstance().scheduleRequest(RequestCode.SCROLL_DOWN, extraData, data -> {});
+    }
+
+
+    private class PointerData {
+        private Position moveStartPos;
+        private Position prevPos;
+
+        private PointerData(Position moveStartPos, Position prevPos) {
+            this.moveStartPos = moveStartPos;
+            this.prevPos = prevPos;
+        }
     }
 
     private class Position {
